d2b5076c0b0b582dcf44e545ff2e3b5acaf39402
vikasrushi <0xvikas@gmail.com>
2022-10-24 15:46:41 +0530

    trending feature added

--- src/componets/Trending.jsx
+++ src/componets/Trending.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../Styles/trending.css";
+import intitaldata from "./intitaldata";
 const Trending = () => {
+  const API_URL = "https://api.coingecko.com/api/v3/search/trending";
+  
+  const [trending, setTrending] = useState(intitaldata);
+
+  const fetchdata = async () => {
+    const req = await fetch(API_URL);
+    const data = await req.json();
+    setTrending(data);
+  };
+  
   return (
     <div className="content">
-      <h1>this is test</h1>
+      <button onClick={fetchdata}>Refresh</button>
+      <div className="list">
+        <ol>
+          {
+           trending.coins.map((prev)=> 
+           <li>
+            <img src={prev.item.small} alt="" />
+            {prev.item.name} - {prev.item.symbol}
+            </li>
+           )
+          }
+        </ol>
+      </div>
     </div>
   );
 };
---
+++ src/componets/intitaldata.js
@@ -0,0 +1,132 @@
+const intitaldata = {
+  coins: [
+    {
+      item: {
+        id: "aptos",
+        coin_id: 26455,
+        name: "Aptos",
+        symbol: "APT",
+        market_cap_rank: 44,
+        thumb:
+          "https://assets.coingecko.com/coins/images/26455/thumb/apt.png?1666581873",
+        small:
+          "https://assets.coingecko.com/coins/images/26455/small/apt.png?1666581873",
+        large:
+          "https://assets.coingecko.com/coins/images/26455/large/apt.png?1666581873",
+        slug: "aptos",
+        price_btc: 0.0004940043548996734,
+        score: 0,
+      },
+    },
+    {
+      item: {
+        id: "freeway",
+        coin_id: 13012,
+        name: "Freeway",
+        symbol: "FWT",
+        market_cap_rank: 870,
+        thumb:
+          "https://assets.coingecko.com/coins/images/13012/thumb/S5h7MHR.png?1652862323",
+        small:
+          "https://assets.coingecko.com/coins/images/13012/small/S5h7MHR.png?1652862323",
+        large:
+          "https://assets.coingecko.com/coins/images/13012/large/S5h7MHR.png?1652862323",
+        slug: "freeway",
+        price_btc: 7.936364378116202e-8,
+        score: 1,
+      },
+    },
+    {
+      item: {
+        id: "dogechain",
+        coin_id: 26828,
+        name: "Dogechain",
+        symbol: "DC",
+        market_cap_rank: 452,
+        thumb:
+          "https://assets.coingecko.com/coins/images/26828/thumb/dogechain.jpeg?1660292741",
+        small:
+          "https://assets.coingecko.com/coins/images/26828/small/dogechain.jpeg?1660292741",
+        large:
+          "https://assets.coingecko.com/coins/images/26828/large/dogechain.jpeg?1660292741",
+        slug: "dogechain",
+        price_btc: 7.083805950590589e-8,
+        score: 2,
+      },
+    },
+    {
+      item: {
+        id: "morpheus-network",
+        coin_id: 2379,
+        name: "Morpheus Network",
+        symbol: "MNW",
+        market_cap_rank: 321,
+        thumb:
+          "https://assets.coingecko.com/coins/images/2379/thumb/MRPH_CoinGecko.png?1635847791",
+        small:
+          "https://assets.coingecko.com/coins/images/2379/small/MRPH_CoinGecko.png?1635847791",
+        large:
+          "https://assets.coingecko.com/coins/images/2379/large/MRPH_CoinGecko.png?1635847791",
+        slug: "morpheus-network",
+        price_btc: 0.00009758270190639453,
+        score: 3,
+      },
+    },
+    {
+      item: {
+        id: "evmos",
+        coin_id: 24023,
+        name: "Evmos",
+        symbol: "EVMOS",
+        market_cap_rank: 68,
+        thumb:
+          "https://assets.coingecko.com/coins/images/24023/thumb/evmos.png?1653958927",
+        small:
+          "https://assets.coingecko.com/coins/images/24023/small/evmos.png?1653958927",
+        large:
+          "https://assets.coingecko.com/coins/images/24023/large/evmos.png?1653958927",
+        slug: "evmos",
+        price_btc: 0.0001050190298072168,
+        score: 4,
+      },
+    },
+    {
+      item: {
+        id: "optimism",
+        coin_id: 25244,
+        name: "Optimism",
+        symbol: "OP",
+        market_cap_rank: 184,
+        thumb:
+          "https://assets.coingecko.com/coins/images/25244/thumb/Optimism.png?1660904599",
+        small:
+          "https://assets.coingecko.com/coins/images/25244/small/Optimism.png?1660904599",
+        large:
+          "https://assets.coingecko.com/coins/images/25244/large/Optimism.png?1660904599",
+        slug: "optimism",
+        price_btc: 0.00004205688260977863,
+        score: 5,
+      },
+    },
+    {
+      item: {
+        id: "stargate-finance",
+        coin_id: 24413,
+        name: "Stargate Finance",
+        symbol: "STG",
+        market_cap_rank: 327,
+        thumb:
+          "https://assets.coingecko.com/coins/images/24413/thumb/STG_LOGO.png?1647654518",
+        small:
+          "https://assets.coingecko.com/coins/images/24413/small/STG_LOGO.png?1647654518",
+        large:
+          "https://assets.coingecko.com/coins/images/24413/large/STG_LOGO.png?1647654518",
+        slug: "stargate-finance",
+        price_btc: 0.000025757352817095395,
+        score: 6,
+      },
+    },
+  ],
+  exchanges: [],
+};
+export default intitaldata;
